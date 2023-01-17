4d65ee8be4ed3907cf096f49d2c6dd16cfcfc568
Shuterix <113360574+Shuterix@users.noreply.github.com>
2023-01-17 07:31:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function showAlert() {
+    let person = prompt("Si pripravenny hrat ? (ano/nie)");
+    if (person == null || person == "") {
+        alert("Nic ste nenapisali.")
+      } else if (person == "ano" || person == "ano ") {
+        alert("Hra zacne po stlaceni tlacitka START. Vela stastia!")
+      } else {
+        alert("Tak si este precitajte navod na hranie.")
+      }
+}
+
+function generateLetter() {
+  const alphabet = 'KPN';
+
+  return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
+
+
+
+function playGame() {
+    let person = prompt("Zadaj K,P alebo N");
+    if(person == null || person == "") {
+        alert("Musis nieco napisat!")
+    } 
+    const pcChoice = generateLetter();
+    console.log(pcChoice)
+    console.log(person)
+    showResult(person, pcChoice);
+}
