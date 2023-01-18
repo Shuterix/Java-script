4993e209698594898b20188d96f28a2fe512e1b8
Shuterix <113360574+Shuterix@users.noreply.github.com>
2023-01-18 17:23:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -27,3 +27,47 @@ function playGame() {
     console.log(person)
     showResult(person, pcChoice);
 }
+
+ function showResult(person, bot) {
+      alert(bot);
+      const result = document.querySelector('.result');
+      const playerScoreBoard = document.querySelector('.p-count');
+      const computerScoreBoard = document.querySelector('.c-count');
+      
+      const resultOptions = ['Vyhral si', 'Prehral si', 'Remiza'];
+	  if(person == 'K' && bot == 'P') {
+        alert("Prehral si")
+        computerScore++;
+        computerScoreBoard.innerHTML = computerScore;
+      } else if(person == 'P' && bot == 'K') {
+        alert("Vyhral si")
+        playerScore++;
+        playerScoreBoard.innerHTML = playerScore;
+      } else if(person === bot) {
+        alert("Remiza")
+      } else if(person == 'P' && bot == 'N') {
+        alert("Prehral si")
+        computerScore++;
+        computerScoreBoard.innerHTML = computerScore;
+      } else if(person == 'N' && bot == 'P') {
+        alert("Vyhral si")
+        playerScore++;
+        playerScoreBoard.innerHTML = playerScore;
+      } else if(person == 'N' && bot == 'K') {
+        alert("Prehral si")
+        computerScore++;
+        computerScoreBoard.innerHTML = computerScore;
+      } else if(person == 'N' && bot == 'N') {
+        alert("Remiza")
+      } else if(person == 'K' && bot == 'K') {
+        alert("Remiza")
+      } else if(person == 'P' && bot == 'P') {
+        alert("Remiza")
+      } else {
+        alert("Vyhral si")
+        playerScore++;
+        playerScoreBoard.innerHTML = playerScore;
+      }
+    //  console.log()   
+    }
+
