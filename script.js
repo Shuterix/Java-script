077809a0ec884550274330c87971c02114336887
Shuterix <113360574+Shuterix@users.noreply.github.com>
2023-01-23 20:44:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,73 +1,74 @@
-function showAlert() {
-    let person = prompt("Si pripravenny hrat ? (ano/nie)");
-    if (person == null || person == "") {
-        alert("Nic ste nenapisali.")
-      } else if (person == "ano" || person == "ano ") {
-        alert("Hra zacne po stlaceni tlacitka START. Vela stastia!")
-      } else {
-        alert("Tak si este precitajte navod na hranie.")
-      }
+function readRules() {
+    let yesNo = prompt("Ste oboznámený s pravidlami ? (áno/nie)")
+    if(yesNo == "áno" || yesNo == "ano" || yesNo == " ano" || yesNo == "ano " || yesNo == "áno " || yesNo == " áno") {
+        actualGameStart();
+    } else {
+        document.getElementById("rules").style.border = "3px solid red";
+        setTimeout(() => {
+            document.getElementById("rules").style.border = "none";
+        }, 800);
+    }
 }
+
+let playerScore = 0;
+    let computerScore = 0;
+
 
 function generateLetter() {
-  const alphabet = 'KPN';
-
-  return alphabet[Math.floor(Math.random() * alphabet.length)]
+    const letters = 'KPN';
+  
+    return letters[Math.floor(Math.random() * letters.length)]
 }
 
 
-
-function playGame() {
-    let person = prompt("Zadaj K,P alebo N");
-    if(person == null || person == "") {
-        alert("Musis nieco napisat!")
-    } 
-    const pcChoice = generateLetter();
-    console.log(pcChoice)
-    console.log(person)
-    showResult(person, pcChoice);
+function actualGameStart() {
+    alert("Hra začína. SKÓRE je :            JA - " + playerScore + "            BOT - " + computerScore);
+    chooseLetterPromt();
 }
 
- function showResult(person, bot) {
-      alert(bot);
-      const result = document.querySelector('.result');
-      const playerScoreBoard = document.querySelector('.p-count');
-      const computerScoreBoard = document.querySelector('.c-count');
-      
-      const resultOptions = ['Vyhral si', 'Prehral si', 'Remiza'];
-	  if(person == 'K' && bot == 'P') {
-        alert("Prehral si")
-        computerScore++;
-        computerScoreBoard.innerHTML = computerScore;
-      } else if(person == 'P' && bot == 'K') {
-        alert("Vyhral si")
-        playerScore++;
-        playerScoreBoard.innerHTML = playerScore;
-      } else if(person === bot) {
-        alert("Remiza")
-      } else if(person == 'P' && bot == 'N') {
-        alert("Prehral si")
-        computerScore++;
-        computerScoreBoard.innerHTML = computerScore;
-      } else if(person == 'N' && bot == 'P') {
-        alert("Vyhral si")
-        playerScore++;
-        playerScoreBoard.innerHTML = playerScore;
-      } else if(person == 'N' && bot == 'K') {
-        alert("Prehral si")
-        computerScore++;
-        computerScoreBoard.innerHTML = computerScore;
-      } else if(person == 'N' && bot == 'N') {
-        alert("Remiza")
-      } else if(person == 'K' && bot == 'K') {
-        alert("Remiza")
-      } else if(person == 'P' && bot == 'P') {
-        alert("Remiza")
-      } else {
-        alert("Vyhral si")
-        playerScore++;
-        playerScoreBoard.innerHTML = playerScore;
-      }
-    //  console.log()   
+function chooseLetterPromt() {
+    let chooseLetter = prompt("Vyber si medzi pismenami K, P alebo N")
+    if(chooseLetter !== "K" && chooseLetter !== "P" && chooseLetter !== "N") {
+        alert("Musite si vybrať medzi písmenami K, P alebo N !!!")
+        chooseLetterPromt();
+    } else {
+      const botPC   = generateLetter();
+      console.log("Toto vybral bot:", botPC);
+      alert("Toto vybral bot: " + botPC);
+      console.log("Toto si si ty vybral", chooseLetter)
+  if(chooseLetter == 'K' && botPC == 'P') {
+      alert("Prehral si")
+      computerScore++;
+      computerScoreBoard.innerHTML = computerScore;
+  } else if(chooseLetter == 'P' && botPC == 'K') {
+      alert("Vyhral si")
+      playerScore++;
+      playerScoreBoard.innerHTML = playerScore;
+  } else if(chooseLetter === botPC) {
+      alert("Remiza")
+  } else if(chooseLetter == 'P' && botPC == 'N') {
+      alert("Prehral si")
+      computerScore++;
+      computerScoreBoard.innerHTML = computerScore;
+  } else if(chooseLetter == 'N' && botPC == 'P') {
+      alert("Vyhral si")
+      playerScore++;
+      playerScoreBoard.innerHTML = playerScore;
+  } else if(chooseLetter == 'N' && botPC == 'K') {
+      alert("Prehral si")
+      computerScore++;
+      computerScoreBoard.innerHTML = computerScore;
+  } else if(chooseLetter == 'N' && botPC == 'N') {
+      alert("Remiza")
+  } else if(chooseLetter == 'K' && botPC == 'K') {
+      alert("Remiza")
+  } else if(chooseLetter == 'P' && botPC == 'P') {
+      alert("Remiza")
+  } else {
+      alert("Vyhral si")
+      playerScore++;
     }
+      
+    }
+}
 
