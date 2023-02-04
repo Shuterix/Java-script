2b1453bc90cbed8853e33a187c584050091461b0
Shuterix <113360574+Shuterix@users.noreply.github.com>
2023-02-04 11:12:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,17 +12,15 @@ function readRules() {
 
 let playerScore = 0;
 let computerScore = 0;
-const computerScoreBoard = document.querySelector('.computer-score-board');
-const playerScoreBoard = document.querySelector('.player-score-board');
-const historyBoard = document.querySelector('.history');
-
+let computerScoreBoard = document.getElementById('computer-score-board');
+let playerScoreBoard = document.getElementById('player-score-board');
+let history = document.getElementById('history');
+history = [];
 
 function generateLetter() {
     const letters = 'KPN';
-  
     return letters[Math.floor(Math.random() * letters.length)]
 }
-
 
 function actualGameStart() {
     alert("Hra začína. SKÓRE je :            JA - " + playerScore + "            BOT - " + computerScore);
@@ -33,72 +31,124 @@ function chooseLetterPromt() {
     let chooseLetter = prompt("Vyber si medzi pismenami K, P alebo N")
     if(chooseLetter !== "K" && chooseLetter !== "P" && chooseLetter !== "N") {
         alert("Musite si vybrať medzi písmenami K, P alebo N !!!")
+
         chooseLetterPromt();
     } else {
       const botPC   = generateLetter();
+
       console.log("Toto vybral bot:", botPC);
+
       alert("Toto vybral bot: " + botPC);
+
       console.log("Toto si si ty vybral", chooseLetter)
-  if(chooseLetter == 'K' && botPC == 'P') {
-      alert("Prehral si")
-      history.pushState("Prehra");
-      computerScore++;
-      count.kamen = count.kamen + 1;
-      count.papier = count.papier + 1;
-      computerScoreBoard.innerHTML = computerScore;
-  } else if(chooseLetter == 'P' && botPC == 'K') {
-      alert("Vyhral si")
-      history.pushState("Vyhra");
-      playerScore++;
-      count.papier = count.papier + 1;
-      count.kamen = count.kamen + 1;
-      playerScoreBoard.innerHTML = playerScore;
-  } else if(chooseLetter === botPC) {
-      alert("Remiza")
-  } else if(chooseLetter == 'P' && botPC == 'N') {
-      alert("Prehral si")
-      history.pushState("Prehra");
-      computerScore++;
-      count.papier = count.papier + 1;
-      count.noznice = count.noznice + 1;
-      computerScoreBoard.innerHTML = computerScore;
-  } else if(chooseLetter == 'N' && botPC == 'P') {
-      alert("Vyhral si")
-      history.pushState("Vyhra");
-      playerScore++;
-      count.noznice = count.noznice + 1;
-      count.papier = count.papier + 1;
-      playerScoreBoard.innerHTML = playerScore;
-  } else if(chooseLetter == 'N' && botPC == 'K') {
-      alert("Prehral si")
-      history.pushState("Prehra");
-      computerScore++;
-      count.noznice = count.noznice + 1;
-      count.kamen = count.kamen + 1;
-      computerScoreBoard.innerHTML = computerScore;
-  } else if(chooseLetter == 'N' && botPC == 'N') {
-      alert("Remiza")
-  } else if(chooseLetter == 'K' && botPC == 'K') {
-      alert("Remiza")
-  } else if(chooseLetter == 'P' && botPC == 'P') {
-      alert("Remiza")
-  } else {
-      alert("Vyhral si")
-      playerScore++;
+      if(chooseLetter == 'K' && botPC == 'P') {
+        alert("Prehral si")
+    
+        count.kamen++;
+        count.papier++;
+        computerScore++;
+        
+        history+=("Prehral si");
+        history.innerHTML = history;
+    
+        computerScoreBoard.innerHTML = computerScore;
+    
+    }else if(chooseLetter == 'K' && botPC == 'N') {
+        alert("Vyhral si")
+    
+        count.kamen++;
+        count.noznice++;
+        playerScore++;
+        
+        history+=("Vyhral si");
+        history.innerHTML = history;
+    
+        playerScoreBoard.innerHTML = playerScore;
+    
+    }else if(chooseLetter == 'K' && botPC == 'K') {
+        alert("Remiza")
+    
+        count.kamen++;
+        count.kamen++;
+        
+        history+=("Remiza");
+        history.innerHTML = history;
+    
+    }else if(chooseLetter == 'P' && botPC == 'N') {
+        alert("Prehral si")
+    
+        count.noznice++;
+        count.papier++;
+        computerScore++;
+        
+        history+=("Prehral si");
+        history.innerHTML = history;
+    
+        computerScoreBoard.innerHTML = computerScore;
+    
+    }else if(chooseLetter == 'P' && botPC == 'K') {
+        alert("Vyhral si")
+    
+        count.kamen++;
+        count.papier++;
+        playerScore++;
+        
+        history+=("Vyhral si");
+        history.innerHTML = history;
+    
+        playerScoreBoard.innerHTML = playerScore;
+    
+    }else if(chooseLetter == 'P' && botPC == 'P') {
+        alert("Remiza")
+    
+        count.papier++;
+        count.papier++;
+        
+        history+=("Remiza");
+        history.innerHTML = history;
+    
+    }else if(chooseLetter == 'N' && botPC == 'K') {
+        alert("Prehral si")
+    
+        count.kamen++;
+        count.noznice++;
+        computerScore++;
+        
+        history+=("Prehral si");
+        history.innerHTML = history;
+    
+        computerScoreBoard.innerHTML = computerScore;
+    
+    }else if(chooseLetter == 'N' && botPC == 'P') {
+        alert("Vyhral si")
+    
+        count.papier++;
+        count.noznice++;
+        playerScore++;
+        
+        history+=("Vyhral si");
+        history.innerHTML = history;
+    
+        playerScoreBoard.innerHTML = playerScore;
+    
+    }else if(chooseLetter == 'N' && botPC == 'N') {
+        alert("Remiza")
+    
+        count.noznice++;
+        count.noznice++;
+        
+        history+=("Remiza");
+        history.innerHTML = history;
+    
     }
-      
     }
+
     console.log("Tolkoto kamenov: " + count.kamen + " Tolkoto papierov: " + count.papier + " Tolkoto noznic: " + count.noznice);
-    historyBoard.innerHTML = history;
+    history = document.getElementById('history').innerHTML = history + "<br/>";
 }
 
-const count = {
+let count = {
     kamen: 0,
     papier: 0,
-    noznice: 0,
-
+    noznice: 0
 };
-
-const history = [];
-
-
